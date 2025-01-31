670f99beb0848797ff38d1de228d273f44228c89
Morry <hm452317@gmail.com>
2025-01-31 10:32:11 +0900

    Make ver 1.0

--- client.py
+++ client.py
@@ -1,4 +1,51 @@
 import socket
+import sys
 
-# ソケットを作成（今回はUDPを採用）
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+# TCP/IPソケットを作成
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# サーバのアドレス
+server_address = '/tmp/socket_file'
+print('connecting to {}'.format(server_address))
+
+# サーバに接続
+# なにか問題があれば、エラーメッセージを表示してプログラムを終了
+try:
+    sock.connect(server_address)
+except socket.error as err:
+    print(err)
+    # プログラムを終了
+    sys.exit(1)
+
+# サーバに接続できたら、サーバにメッセージを送信
+try:
+    # 送信するメッセージを入力するよう求める
+    message = input('Enter the message to send to the server >>> ')
+    # バイト型に変換
+    message = message.encode()
+    sock.sendall(message)
+
+    # サーバからの応答を待つ時間を2秒間に設定
+    # この時間が過ぎても応答がない場合、プログラムは次のステップに進む。
+    sock.settimeout(2)
+
+    # サーバからの応答を待ち、応答があればそれを表示
+    try:
+        while True:
+            # サーバからのデータを受け取る（受け取るデータ量の最大量は32バイトに設定）
+            data = str(sock.recv(32))
+
+            # データがあればそれを表示、なかったらループを終了
+            if data:
+                print(data)
+            else:
+                break
+    
+    # 2秒間サーバからの応答がなければ、タイムアウトエラーとなり、エラーメッセージを表示する
+    except(TimeoutError):
+        print('Socket timeout, ending listening for server messages')
+    
+# すべての操作が完了したら、最後にソケットを閉じて通信を終了
+finally:
+    print('closing socket')
+    sock.close()
---
+++ server.py
@@ -0,0 +1,70 @@
+import socket
+import os
+from faker import Faker
+
+# UNIXソケットをストリームモードで作成
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# このサーバが接続を待つUNIXソケットのパスを設定
+server_address = '/tmp/socket_file'
+
+# 以前の接続が残っていた場合に備えて、サーバアドレスをアンリンク（削除）する
+try:
+    os.unlink(server_address)
+# サーバアドレスが存在しない場合は例外を無視
+except FileNotFoundError:
+    pass
+
+# サーバアドレスにソケットをバインド（接続）する
+sock.bind(server_address)
+
+# ソケットに接続要求を待機させる
+sock.listen(1)
+
+# 無限ループでクライアントからの接続を待ち続ける
+while True:
+    # クライアントからの接続を受け入れる
+    connection, client_address = sock.accept()
+    try:
+        print('connection from', client_address)
+
+        # サーバが新しいデータを待ち続けるためのループ
+        while True:
+            # ここでサーバは接続からデータを読み込む
+            # 一度に最大16バイト読み込む
+            data = connection.recv(16)
+
+            # 受け取ったデータをバイナリ形式から文字列に変換(utf-8)
+            data_str = data.decode('utf-8')
+
+            # 受け取ったデータを表示
+            print('Received ' + data_str)
+
+            if data:
+                fake = Faker()
+                # 返信をからの文字列で初期化
+                response = ''
+
+                if data_str == "What's your name?":
+                    response = 'My name is ' + fake.name()
+                elif data_str == "What is your job?":
+                    response = 'My job is ' + fake.job()
+                elif data_str == "When is your birthday?":
+                    response = 'My birthday is ' + fake.date(pattern='%Y/%m/%d')
+                elif data_str == "Where are you from?":
+                    response = "I'm form " + fake.country()
+                else:
+                    response = "Sorry, I can't answer this question. Ask me something else..."
+
+                # メッセージをバイナリ形式にしてクライアントに送り返す
+                connection.sendall(response.encode())
+            
+            # クライアントからデータが送られてこなければ、ループを終了
+            else:
+                print('no data from', client_address)
+                break
+
+    # 最終的に接続を閉じる
+    finally:
+        print('Closing current connection')
+        connection.close()
